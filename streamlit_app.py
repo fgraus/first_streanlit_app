659cf890be03c4aec456fde0e48cff69c929285c
Fernando Graus <fggraus@gmail.com>
2023-04-06 09:50:23 -0500

    Update streamlit_app.py
    All the menu with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,12 @@ import streamlit
 
 streamlit.title('My parents, new healthy dinner')
 
-streamlit.header('Breakfast menu')
 
-streamlit.text('Omega 3 & Blueberry something')
-streamlit.text('Kale, Spinach & Rocket smoothy')
-streamlit.text('Hard-boiled Free-Range eggs')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
